3c340b4e2490c244591d0dc446518f01d0f2e2b9
DarinaKochesheva <darinakochesheva930@gmail.com>
2026-03-20 20:08:18 +0300

    i update static

--- part1/part1_1/BankAccount.java
+++ part1/part1_1/BankAccount.java
@@ -38,7 +38,7 @@ public class BankAccount {
     static {
         // TODO: bankName = "Java Bank"; выведите "Банковская система инициализирована"
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-
+        bankName = "Java Bank";
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
